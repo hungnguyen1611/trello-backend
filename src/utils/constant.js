c70fe24c583090e230371096174ab60e94b111f4
hungnguyen16112003 <nguyenquochung16112003@gmail.com>
2025-06-02 18:33:25 +0700

    updated list_domain

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,9 @@
 const { env } = require("@/configs/environment");
 
-const WHITELIST_DOMAINS = ["https://hungnguyen1611.online"];
+const WHITELIST_DOMAINS = [
+  "https://trello-web-git-master-hungnguyen1611s-projects.vercel.app",
+  "https://www.hungnguyen1611.online",
+];
 
 const BOARD_TYPE = {
   PUBLIC: "public",
